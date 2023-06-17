5ed1a59b0ffff23efe59173b814290ef8cabd1bc
Myrza Alymbekov <myrza-alymbekov@MacBook-Air-Myrza.local>
2023-06-17 17:53:01 +0600

    a

--- app/app_feedback/admin.py
+++ app/app_feedback/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-<<<<<<< HEAD
 from .models import Feedback, Application
 
 admin.site.register(Feedback)
 admin.site.register(Application)
-
-=======
-
 admin.site.register(QuestionAnswer)
-admin.site.register(Feedback)
 
->>>>>>> 2c9786bc27d72e546309787869ec514a31a46918
