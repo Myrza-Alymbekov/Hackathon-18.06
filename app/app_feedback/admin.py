b8c6c99ef5694a9404e7fcaba9b99923339f97c9
ukuvanychbekov <u.kuvanychbekov@gmail.com>
2023-06-17 20:41:34 +0600

    added comment

--- app/app_feedback/admin.py
+++ app/app_feedback/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-from .models import Feedback, Application
+from .models import Feedback, Application, FeedbackComments
 
 admin.site.register(Feedback)
 admin.site.register(Application)
 admin.site.register(QuestionAnswer)
+admin.site.register(FeedbackComments)
 
