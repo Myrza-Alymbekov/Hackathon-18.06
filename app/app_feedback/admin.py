59e29612f0ce11cb5843f5b61f56150219fea926
Myrza Alymbekov <myrza-alymbekov@MacBook-Air-Myrza.local>
2023-06-17 20:36:29 +0600

    added donated summ

--- app/app_feedback/admin.py
+++ app/app_feedback/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-from .models import Feedback, Application
 
 admin.site.register(Feedback)
+admin.site.register(FeedbackFiles)
+admin.site.register(FeedbackComments)
 admin.site.register(Application)
+admin.site.register(ApplicationFiles)
+admin.site.register(ApplicationComments)
 admin.site.register(QuestionAnswer)
+admin.site.register(Requisite)
+admin.site.register(Donation)
 
